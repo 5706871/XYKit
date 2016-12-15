eda2e572ab512ebf0d29c482c5430952d706a8ae
XY <5706871@qq.com>
2016-12-15 11:54:05 +0800

    初始化项目

---
+++ XYKit/XYKit/XYKit.h
@@ -0,0 +1,19 @@
+//
+//  XYKit.h
+//  XYKit
+//
+//  Created by fatty on 2016/12/15.
+//  Copyright © 2016年 xy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XYKit.
+FOUNDATION_EXPORT double XYKitVersionNumber;
+
+//! Project version string for XYKit.
+FOUNDATION_EXPORT const unsigned char XYKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYKit/PublicHeader.h>
+
+
